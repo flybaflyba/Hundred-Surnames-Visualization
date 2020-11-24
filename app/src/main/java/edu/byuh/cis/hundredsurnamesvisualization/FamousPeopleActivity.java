3a4edb621f37ca697146f5d94f78e2fa2b528772
Esther <lamtszchinesther@gmail.com>
2020-11-24 13:07:09 +0800

    added layout

--- app/src/main/java/edu/byuh/cis/hundredsurnamesvisualization/FamousPeopleActivity.java
+++ app/src/main/java/edu/byuh/cis/hundredsurnamesvisualization/FamousPeopleActivity.java
@@ -1,13 +1,11 @@
 package edu.byuh.cis.hundredsurnamesvisualization;
 
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,38 +29,24 @@ public class FamousPeopleActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         final String person=dataHolder.famousPeopleList.get(personID);
         webView.loadUrl("https://en.wikipedia.org/wiki/"+person);
-//        LinearLayout.LayoutParams nice = new LinearLayout.LayoutParams
-//                (LinearLayout.LayoutParams.MATCH_PARENT,
-//                        LinearLayout.LayoutParams.MATCH_PARENT, 1);
-//
-//
-//        TextView tv=new TextView(this);
-//        tv.setText(personID);
-//
-//        Button prevButton=new Button(this);
-//        Button nextButton=new Button(this);
-//
-//        prevButton.setText("<<");
-//        nextButton.setText(">>");
-//
-//        LinearLayout lnl=new LinearLayout(this);
-//        lnl.setOrientation(LinearLayout.VERTICAL);
-//        //lnl.addView(webView);
-//        lnl.addView(tv);
-//        lnl.addView(prevButton);
-//        lnl.addView(nextButton);
-//
-//        setContentView(lnl);
+
+        TextView textView = findViewById(R.id.title);
+        textView.setText(person);
+
+        Button buttonLeft = findViewById(R.id.buttonLeft);
+        buttonLeft.setText(">>");
+        //buttonLeft.setOnClickListener();
 
 
-    }
-    @Override
-    public boolean onTouchEvent(MotionEvent m){
-        if(m.getAction()==MotionEvent.ACTION_DOWN){
-            personID+=1;
-        }
-        return true;
-    }
+
+            }
+//    @Override
+//    public boolean onTouchEvent(MotionEvent m){
+//        if(m.getAction()==MotionEvent.ACTION_DOWN){
+//            personID+=1;
+//        }
+//        return true;
+//    }
 
 
     @Override
