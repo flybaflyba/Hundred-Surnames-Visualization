d4d9b15dc8a0d3b0b6323be00807764fadfc6ff1
Esther <lamtszchinesther@gmail.com>
2020-11-24 11:21:07 +0800

    add webview

--- app/src/main/java/edu/byuh/cis/hundredsurnamesvisualization/FamousPeopleActivity.java
+++ app/src/main/java/edu/byuh/cis/hundredsurnamesvisualization/FamousPeopleActivity.java
@@ -1,17 +1,36 @@
 package edu.byuh.cis.hundredsurnamesvisualization;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FamousPeopleActivity extends AppCompatActivity {
 
+
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.famous_people_view);
 
-        //let make some changes lol
+        webView=(WebView)findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://en.wikipedia.org/wiki/Lin_(surname)");
+
     }
+
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
 
 }
