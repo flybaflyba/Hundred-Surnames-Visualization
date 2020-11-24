f0f80aafa8fd845e4e4d686d73b74eaa3a0a5baf
Esther <lamtszchinesther@gmail.com>
2020-11-24 12:29:34 +0800

    added arrayList

--- app/src/main/java/edu/byuh/cis/hundredsurnamesvisualization/FamousPeopleActivity.java
+++ app/src/main/java/edu/byuh/cis/hundredsurnamesvisualization/FamousPeopleActivity.java
@@ -1,9 +1,14 @@
 package edu.byuh.cis.hundredsurnamesvisualization;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,24 +17,51 @@ public class FamousPeopleActivity extends AppCompatActivity {
 
     private WebView webView;
     DataHolder dataHolder;
+    private int personID;
 
-
-
-
-    
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.famous_people_view);
-        DataHolder dataHolder = new DataHolder(this);
-
+        final DataHolder dataHolder = new DataHolder(this);
+        personID=0;
         webView=(WebView)findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
-        String person=dataHolder.famousPeopleList.get(0);
+        final String person=dataHolder.famousPeopleList.get(personID);
         webView.loadUrl("https://en.wikipedia.org/wiki/"+person);
+//        LinearLayout.LayoutParams nice = new LinearLayout.LayoutParams
+//                (LinearLayout.LayoutParams.MATCH_PARENT,
+//                        LinearLayout.LayoutParams.MATCH_PARENT, 1);
+//
+//
+//        TextView tv=new TextView(this);
+//        tv.setText(personID);
+//
+//        Button prevButton=new Button(this);
+//        Button nextButton=new Button(this);
+//
+//        prevButton.setText("<<");
+//        nextButton.setText(">>");
+//
+//        LinearLayout lnl=new LinearLayout(this);
+//        lnl.setOrientation(LinearLayout.VERTICAL);
+//        //lnl.addView(webView);
+//        lnl.addView(tv);
+//        lnl.addView(prevButton);
+//        lnl.addView(nextButton);
+//
+//        setContentView(lnl);
 
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent m){
+        if(m.getAction()==MotionEvent.ACTION_DOWN){
+            personID+=1;
+        }
+        return true;
     }
 
 
